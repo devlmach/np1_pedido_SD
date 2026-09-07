6ea9c6db4c5d51b9de31626861f3df96c07e0816
devlmach <machiavelli.rafa@gmail.com>
2026-09-07 15:30:13 -0300

    feat: implement middleware and custom exceptions

---
+++ trabalho_np1_pedido/Common/Middleware/ExceptionsMiddeware.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using System.Text.Json;
+using trabalho_np1_pedido.Domain.Errors;
+
+namespace trabalho_np1_pedido.Common.Middleware
+{
+    public class ExceptionsMiddeware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionsMiddeware> _logger;
+
+        public ExceptionsMiddeware(RequestDelegate requestDelegate, ILogger<ExceptionsMiddeware> logger)
+        {
+            _next = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+
+                var statusCode = ex switch
+                {
+                    BadRequestException => (int)HttpStatusCode.BadRequest,
+                    NotFoundException => (int)HttpStatusCode.NotFound,
+                    DomainExceptions => (int)HttpStatusCode.BadRequest,
+
+                    _ => StatusCodes.Status500InternalServerError
+                };
+
+                context.Response.StatusCode = statusCode;
+
+                var response = new ErrorResponse
+                {
+                    StatusCode = statusCode,
+                    Message = ex.Message
+                };
+
+                JsonSerializerOptions jsonSerializerOptions = new()
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+                var options = jsonSerializerOptions;
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response, options));
+            }
+        }
+    }
+
+    public class ErrorResponse
+    {
+        public int StatusCode { get; set; }
+        public required string Message { get; set; }
+        public IEnumerable<ErrorDetailResponse>? Details { get; set; }
+    }
+
+    public class ErrorDetailResponse
+    {
+        public required string Field { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ trabalho_np1_pedido/Domain/Errors/DomainExceptions.cs
@@ -0,0 +1,27 @@
+﻿namespace trabalho_np1_pedido.Domain.Errors
+{
+    public class DomainExceptions : Exception
+    {
+        public DomainExceptions(string message) : base(message) { }
+    }
+
+    public class NotFoundException : DomainExceptions
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+
+    public class BadRequestException : DomainExceptions
+    {
+        public BadRequestException(string message) : base(message) { }
+    }
+
+    public class ForbiddenException : DomainExceptions
+    {
+        public ForbiddenException(string messase) : base(messase) { }
+    }
+
+    public class NullException : DomainExceptions
+    {
+        public NullException(string message) : base(message) { }
+    }
+}
