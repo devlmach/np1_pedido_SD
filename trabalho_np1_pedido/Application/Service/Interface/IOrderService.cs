3f30cd3c8c68d7e3f5889d1729c144a106e5b837
devlmach <machiavelli.rafa@gmail.com>
2026-09-07 15:35:06 -0300

    feat: implement orderservice class

---
+++ trabalho_np1_pedido/Application/Service/Interface/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using trabalho_np1_pedido.Application.Dto;
+
+namespace trabalho_np1_pedido.Application.Service.Interface
+{
+    public interface IOrderService
+    {
+        Task<OrderItemDto> GetOrderByIdAsync(long id);
+        Task<List<OrderItemDto>> GetAllOrdersAsync();
+        Task CreateOrderAsync(OrderItemAddDto orderItemAddDto);
+        Task UpdateOrderAsync(long id, OrderItemUpdateDto orderItemUpdateDto);
+        Task DeleteOrderAsync(long id);
+    }
+}
---
+++ trabalho_np1_pedido/Application/Service/OrderService.cs
@@ -0,0 +1,52 @@
+﻿using trabalho_np1_pedido.Application.Dto;
+using trabalho_np1_pedido.Application.Service.Interface;
+using trabalho_np1_pedido.Data.Repository.Interface;
+using trabalho_np1_pedido.Domain.Errors;
+
+namespace trabalho_np1_pedido.Application.Service
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task CreateOrderAsync(OrderItemAddDto orderItemAddDto)
+        {
+            await _orderRepository.CreateOrderAsync(orderItemAddDto);
+        }
+
+        public async Task DeleteOrderAsync(long id)
+        {
+            var _ = await _orderRepository.GetOrderByIdAsync(id)
+                ?? throw new NotFoundException("Order not found");
+
+            await _orderRepository.DeleteOrderAsync(id);
+        }
+
+        public async Task<List<OrderItemDto>> GetAllOrdersAsync()
+        {
+            var orders = await _orderRepository.GetAllOrdersAsync();
+            return orders;
+        }
+
+        public async Task<OrderItemDto> GetOrderByIdAsync(long id)
+        {
+            var order = await _orderRepository.GetOrderByIdAsync(id)
+                ?? throw new NotFoundException("Order not found");
+
+            return order;
+        }
+
+        public async Task UpdateOrderAsync(long id, OrderItemUpdateDto orderItemUpdateDto)
+        {
+            var _ = await _orderRepository.GetOrderByIdAsync(id)
+                ?? throw new NotFoundException("Order not found");
+
+            await _orderRepository.UpdateOrderAsync(id, orderItemUpdateDto);
+        }
+    }
+}
